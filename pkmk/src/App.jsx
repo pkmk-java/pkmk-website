feb09566454aeb2fefef0030e73f82fc1d3adc69
djamet <kampretsiji1@gmail.com>
2024-06-05 23:34:18 +0700

    add button

--- pkmk/src/App.jsx
+++ pkmk/src/App.jsx
@@ -3,6 +3,7 @@ export default function App() {
     <div className=" bg-black h-screen w-screen text-white">
       <h1>Hello pkmk</h1>
       <h2>Test branch</h2>
+      <button className=" bg-slate-50 w-[100px] text-black">login</button>
     </div>
   );
 }
