fdc0f452f3eb6bf7105aa3590aea2f83dec74f3f
djamet <kampretsiji1@gmail.com>
2024-06-06 11:32:01 +0700

    admin model

--- backend/model/adminModel.js
+++ backend/model/adminModel.js
@@ -9,5 +9,29 @@ const adminSchema = new Schema({
   email: {
     type: String,
     required: [true, "please provide email"],
+    unique: true,
   },
+  password: {
+    type: String,
+    min: 6,
+    required: [true, "please provide password"],
+  },
+  avatar: {
+    type: String,
+    default: "",
+  },
+  role: {
+    enum: ["admin", "member"],
+    default: "admin",
+  },
+  createdProdcut: [
+    {
+      productId: {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    },
+  ],
 });
+
+export const adminModel = mongoose.model("Admin", adminSchema);
