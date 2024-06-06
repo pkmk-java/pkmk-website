36162558ed0681d55fc2ae2e72562083606b6a27
djamet <kampretsiji1@gmail.com>
2024-06-06 17:25:29 +0700

    admin controller update

--- backend/helper/passwordCompare.js
+++ backend/helper/passwordCompare.js
@@ -1,1 +1,5 @@
-import bcrypt from "";
+import bcrypt from "bcrypt";
+
+export const passwordCompareHandler = ({ userPass, dataPass }) => {
+  return bcrypt.compare(userPass, dataPass);
+};
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { loginAdmin, registerAdmin } from "../controller/adminController.js";
+import { adminMiddleware } from "../middleware/adminMiddlware.js";
+import { upload } from "../middleware/multer.js";
 const router = express.Router();
 
 router.post("/register", registerAdmin);
 router.post("/login", loginAdmin);
+router.post("/create-product", adminMiddleware, upload.single("image"));
 
 export const adminRouter = router;
