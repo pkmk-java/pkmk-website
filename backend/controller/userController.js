4feaa653df98ed45ef6ab45739efd74e5267fb5d
djamet <kampretsiji1@gmail.com>
2024-06-07 22:21:45 +0700

    changes

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -70,9 +70,8 @@ const loginUser = async (req, res) => {
       .status(200)
       .cookie("token", token, {
         expires: new Date(Date.now() + 900000),
-        httpOnly: true,
       })
-      .json({ msg: "success login user", isUserExist });
+      .json({ msg: "success login user", isUserExist, token });
   } catch (error) {
     console.log(error);
     return res.status(501).json({ msg: "internal server error" });
